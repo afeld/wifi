bf57a6eda1eed0a1d76c46d41459948cfb245fba
Aidan Feldman <aidan.feldman@gmail.com>
2014-01-22 05:11:58 -0500

    search for nearby venues and print them to the console

--- wifi.js
+++ wifi.js
@@ -1,5 +1,11 @@
 (function(){
+  // https://foursquare.com/developers/app/IBFUDPTFRDXZDBZJB4FZQ4QWDPPCTDRWWMCB0XXWNLW524ZD
+  var CLIENT_ID = 'IBFUDPTFRDXZDBZJB4FZQ4QWDPPCTDRWWMCB0XXWNLW524ZD';
+  var CLIENT_SECRET = 'V1NS4BZV014GW5HK0JH2N1K3ZANXMOBEI0EIUG02SWRU3BO4';
+
   var getGeolocation = function() {
+    var deferred = $.Deferred();
+
     var options = {
       enableHighAccuracy: true,
       timeout: 5000,
@@ -13,17 +19,41 @@
       console.log('Latitude : ' + crd.latitude);
       console.log('Longitude: ' + crd.longitude);
       console.log('More or less ' + crd.accuracy + ' meters.');
+
+      deferred.resolve(crd);
     };
 
     var onError = function(err) {
       console.warn('ERROR(' + err.code + '): ' + err.message);
+      deferred.reject(err)
     };
 
     navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+
+    return deferred.promise();
   };
 
+  var getVenues = function(coords) {
+    var url = 'https://api.foursquare.com/v2/venues/search';
+
+    return $.getJSON(url, {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      v: 20140122,
+      q: 'wi-fi',
+      ll: coords.latitude + ',' + coords.longitude
+    });
+  };
+
+  var displayVenues = function(data) {
+    console.log(data);
+  };
+
+
   if ("geolocation" in navigator) {
-    getGeolocation();
+    getGeolocation().
+      then(getVenues).
+      then(displayVenues);
   } else {
     alert("Your browser doesn't support geolocation. Sorry!");
   }
